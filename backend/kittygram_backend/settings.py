df8042be52a2018fb33ee918a50860003ba98609
FrolovAlex22 <frolov.bsk@yandex.ru>
2024-01-19 16:26:22 +0400

    FP step1 03

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.23.84', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.250.23.84', '127.0.0.1', 'localhost', 'projectkittygram.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -98,6 +98,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
